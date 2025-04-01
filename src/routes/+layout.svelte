ce4d605db1d956efc8113902fbd14ad4e964975e
Dawson Zhou <dawson.m.zhou@gmail.com>
2025-04-02 00:24:46 +0200

    Nav bar for subpages.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
-	import '../app.css';
-	
-	let { children } = $props();
+    import '../app.css';
+    
+    let { children } = $props();
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/modules">Modules</a>
-</nav>
+<div class="navbar bg-base-200 shadow-sm">
+    <!-- Small screen dropdown menu for subpages. -->
+    <div class="navbar-start">
+        <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+            </div>
+            <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li><a href="/">Home</a></li>
+                <li><a href="/modules">Modules</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- Large screen center links for subpages. -->
+    <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+            <li><a href="/">Home</a></li>
+            <li><a href="/modules">Modules</a></li>
+        </ul>
+    </div>
+
+    <!-- Unused, but kept in for spacing. -->
+    <div class="navbar-end"></div>
+</div>
 
 {@render children()}
--- src/routes/modules/+page.svelte
+++ src/routes/modules/+page.svelte
@@ -3,18 +3,17 @@
 </script>
 
 {#if data.modules && data.modules.length}
-<ul class="list bg-base-100 rounded-box shadow-md">
-    <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Beginners Training Modules</li>
-    {#each data.modules as module}
-    <li class="list-row">
-        <div class="text-6xl font-bold">{module.order}</div>
-        <div class="rich-text">
-            <div class="text-sm uppercase font-semibold">{module.title}</div>
-            {@html module.text_html}
-        </div>
-    </li>
-    {/each}
-</ul>
+    <ul class="list bg-base-100 rounded-box shadow-md m-10">
+        <li class="p-4 pb-2 text-xl font-semibold tracking-wide">Beginners Training Modules</li>
+        {#each data.modules as module}
+        <li class="list-row">
+            <div class="rich-text">
+                <div class="text-sm uppercase font-semibold">{module.title}</div>
+                {@html module.text_html}
+            </div>
+        </li>
+        {/each}
+    </ul>
 {:else}
-  <div>No data found.</div>
+    <div>No data found.</div>
 {/if}
