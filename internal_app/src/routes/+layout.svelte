d8610725c4b2d8e149b76b3e2a25c216254c8077
Dawson Zhou <dawson.m.zhou@gmail.com>
2025-04-18 19:30:21 +0200

    Fix layout for mobile screens.

--- internal_app/src/routes/+layout.svelte
+++ internal_app/src/routes/+layout.svelte
@@ -7,21 +7,53 @@
     const route = page.route.id;
 </script>
 
-<div class="flex justify-center border-b-1 border-gray-300 font-bold text-lg pb-2 mb-4">
-    <div class="flex justify-center items-center relative size-40">
-        <a class="{route === '/about' ? 'text-blue-800' : 'text-blue-500'} hover:text-blue-300" href="/about" data-sveltekit-reload>About Us</a>
+<div class="drawer">
+    <input id="nav-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col">
+        <div class="flex justify-center border-b-1 border-gray-300 font-bold text-lg pb-2 my-2">
+            <!-- Hamburger menu with side-drawer for navigation on small screens. Logo still shows. -->
+            <div class="flex md:hidden items-center mx-2">
+                <label for="nav-drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M 4,6  h 16
+                                M 4,12 h 16
+                                M 4,18 h 16" />
+                    </svg>
+                </label>
+            </div>
+
+            <!-- Normal navigation on medium or larger screens. -->
+            <div class="flex max-md:hidden justify-center items-center relative size-40">
+                <a class="{route === '/about' ? 'text-blue-800' : 'text-blue-500'} hover:text-blue-300" href="/about" data-sveltekit-reload>About Us</a>
+            </div>
+            <div class="flex max-md:hidden justify-center items-center relative size-40">
+                <a class="{route === '/beginners' ? 'text-blue-800' : 'text-blue-500'} hover:text-blue-300" href="/beginners" data-sveltekit-reload>Beginners</a>
+            </div>
+            <div class="flex max-md:grow justify-center items-center relative size-40 max-md:size-20 ml-5 mr-5">
+                <a href="/" data-sveltekit-reload><img class="w-140 h-140 max-md:w-20 max-md:h-20" src={logo} alt="Oslo Roller Derby logo" /></a>
+            </div>
+            <div class="flex max-md:hidden justify-center items-center relative size-40">
+                <a class="{route?.startsWith('/beginners/plans') ?? false ? 'text-blue-800' : 'text-blue-500'} hover:text-blue-300" href="/beginners/plans" data-sveltekit-reload>Training Plans</a>
+            </div>
+            <div class="flex max-md:hidden justify-center items-center relative size-40">
+                <a class="{route === '/beginners/modules' ? 'text-blue-800' : 'text-blue-500'} hover:text-blue-300" href="/beginners/modules" data-sveltekit-reload>All Modules</a>
+            </div>
+
+            <!-- For symmetric spacing with the dropdown menu on the left side on small screens. -->
+            <div class="flex md:hidden h-8 w-8 mx-2"></div>
+        </div>
     </div>
-    <div class="flex justify-center items-center relative size-40">
-        <a class="{route === '/beginners' ? 'text-blue-800' : 'text-blue-500'} hover:text-blue-300" href="/beginners" data-sveltekit-reload>Beginners</a>
-    </div>
-    <div class="flex justify-center items-center relative size-40 ml-5 mr-5">
-        <a href="/" data-sveltekit-reload><img src={logo} width="140" height="140" alt="Oslo Roller Derby logo" /></a>
-    </div>
-    <div class="flex justify-center items-center relative size-40">
-        <a class="{route?.startsWith('/beginners/plans') ?? false ? 'text-blue-800' : 'text-blue-500'} hover:text-blue-300" href="/beginners/plans" data-sveltekit-reload>Training Plans</a>
-    </div>
-    <div class="flex justify-center items-center relative size-40">
-        <a class="{route === '/beginners/modules' ? 'text-blue-800' : 'text-blue-500'} hover:text-blue-300" href="/beginners/modules" data-sveltekit-reload>All Modules</a>
+
+    <!-- Side-drawer content. -->
+    <div class="drawer-side z-50">
+        <label for="nav-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu bg-base-200 min-h-full w-60 p-4">
+            <li><a href="/about" data-sveltekit-reload>About Us</a></li>
+            <li><a href="/beginners" data-sveltekit-reload>Beginners</a></li>
+            <li><a href="/beginners/plans" data-sveltekit-reload>Training Plans</a></li>
+            <li><a href="/beginners/modules" data-sveltekit-reload>All Modules</a></li>
+        </ul>
     </div>
 </div>
 
--- internal_app/src/routes/beginners/plans/+page.svelte
+++ internal_app/src/routes/beginners/plans/+page.svelte
@@ -7,7 +7,7 @@
         {#each data.seasons as season}
             {#if data.training_plan_summaries.has(season._id)}
                 <div class="text-4xl font-semibold my-5">{season.display_text}</div>
-                <div class="grid grid-cols-3 gap-10 mx-50 my-5">
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 mx-5 sm:mx-10 md:mx-20 xl:mx-30">
                     {#each data.training_plan_summaries.get(season._id)! as trainingPlanSummary}
                         <a href="/beginners/plans/{season.short_text}-{trainingPlanSummary.lesson_number}" class="rounded-xl border-2 border-gray-100 hover:border-gray-300 shadow-2xl p-5">
                             <div class="text-3xl font-semibold mb-4">Lesson {trainingPlanSummary.lesson_number}</div>
