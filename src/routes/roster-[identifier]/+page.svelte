4dbe75faf192384329bf0376d5dc068102aa062d
Dawson Zhou <dawson.m.zhou@gmail.com>
2025-09-26 01:38:05 +0200

    Fix roster layout for mobile.

--- src/routes/roster-[identifier]/+page.svelte
+++ src/routes/roster-[identifier]/+page.svelte
@@ -16,11 +16,11 @@
   <div class="text-xl font-semibold">{roster.name}</div>
   <div class="text-lg subheading">{roster.season}</div>
   
-  <div class="flex flex-row gap-8">
+  <div class="flex flex-col sm:flex-row gap-8">
     <SkaterNumberGroup heading={roster.line_a_name.valueOf()} skaterNumbers={roster.line_a} />
     <SkaterNumberGroup heading={roster.line_b_name.valueOf()} skaterNumbers={roster.line_b} />
   </div>
-  <div class="flex flex-row gap-8 mt-4">
+  <div class="flex flex-col sm:flex-row gap-8 mt-4">
     {#if roster.jammers}
       <SkaterNumberGroup heading="Jammers" skaterNumbers={roster.jammers} />
     {/if}
