ecb3ee3744efbdc5a98f247f68cb63a5ddf0afce
Dawson Zhou <dawson.m.zhou@gmail.com>
2025-11-06 00:05:47 +0100

    Add feedback button to updates.

--- src/routes/updates/+page.svelte
+++ src/routes/updates/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { page } from '$app/state';
-  import { Link } from '@lucide/svelte/icons';
+  import { Dot, Link } from '@lucide/svelte/icons';
   import { PortableText } from '@portabletext/svelte';
   import { Crumb, CrumbHome, CrumbPage, CrumbSeparator } from '$lib/components/breadcrumb/index';
+  import FeedbackDialog from '$lib/components/feedback_dialog.svelte';
   import PortableNormal from '$lib/components/portable_text/portable_normal.svelte';
   import PortablePostImage from '$lib/components/portable_text/portable_post_image.svelte';
   import { formatDateTextWithYear } from '$lib/util/datetime';
 
-  let { data } = $props();
+  let { data, form } = $props();
   const showHidden = $derived(page.url.searchParams.get('show-hidden')?.trim().toLowerCase() === 'true');
   const hiddenQuery = $derived(showHidden ? '?show-hidden=true' : '');
 
@@ -24,17 +25,20 @@
 </Crumb>
 
 <div class="flex flex-col gap-1 bottom-faded-border">
-  {#each data.posts as post}
+  {#each data.posts as post, index}
     <div id={post.slug} class="text-2xl sm:text-3xl font-semibold">
       {post.title}
     </div>
-    <div class="flex gap-3 items-center">
+    <div class="flex gap-1 items-center">
       <div class="text-lg sm:text-xl subheading">
         {formatDateTextWithYear(post.date)}
       </div>
+      <Dot />
       <a class="link" href="/updates{hiddenQuery}#{post.slug}">
         <Link size={20}/>
       </a>
+      <Dot />
+      <FeedbackDialog context="Update: {post.title}" {form} formId="update-{index}" />
     </div>
     <div class="text-md italic">By {post.author}</div>
     {#if !post.visible}
